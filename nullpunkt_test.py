d1d6ebf005435ddb8ab5c62367eb1c2515dc177a
Jon Bjarne Bø <77270569+Z0XZ@users.noreply.github.com>
2023-11-24 14:26:42 +0100

    Update nullpunkt_test.py

--- nullpunkt_test.py
+++ nullpunkt_test.py
@@ -8,20 +8,24 @@ import math
 
 def generer_tilfeldige_koeffisienter():
     """ Hjelpefunksjon for å generere tilfeldige koeffisienter. """
-    a = random.uniform(-10, 10)
-    b = random.uniform(-10, 10)
-    c = random.uniform(-10, 10)
+    a = random.randint(-100, 100)/10
+    if round(a, 2) == 0:
+        a += 1
+    b = random.randint(-100, 100)/10
+    c = random.randint(-100, 100)/10
+    if random.randint(1,20) == 1:
+        c = b**2 / (4*a)
     return a, b, c
-
+    
 @pytest.mark.parametrize("a,b,c", [generer_tilfeldige_koeffisienter() for _ in range(100)])
 def test_tilfeldige_nullpunkt(a, b, c):
     """ Tester nullpunkt-funksjonen med tilfeldig genererte koeffisienter. """
     discriminant = b**2 - 4*a*c
     expected = "Funksjonen har ingen nullpunkter!"
     if discriminant > 0:
-        expected = ((-b + math.sqrt(discriminant)) / (2 * a), 
-                    (-b - math.sqrt(discriminant)) / (2 * a))
+        expected = (round((-b + math.sqrt(discriminant)) / (2 * a),2), 
+                    round((-b - math.sqrt(discriminant)) / (2 * a),2))
     elif discriminant == 0:
-        expected = (-b / (2 * a),)
-
+        expected = round(-b / (2 * a), 2)
+    
     assert nullpunkt.nullpunkt(a, b, c) == expected
